e988f905a3dbaced7d8529780799598168eae205
Kyungeun <ke.kevin.kim@gmail.com>
2022-05-28 13:39:13 +0900

    Change @Getter and @Setter to @Data

--- src/main/java/kevin/study/blogmongowebflux/mongo/DatabaseSequence.java
+++ src/main/java/kevin/study/blogmongowebflux/mongo/DatabaseSequence.java
@@ -1,12 +1,10 @@
 package kevin.study.blogmongowebflux.mongo;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
+@Data
 @Document(collection = "database_sequences")
 public class DatabaseSequence {
 
